f89264ef9e66b7c7b1530214e98abdd1080d5e43
aTegart <27782045+aTegart@users.noreply.github.com>
2020-01-28 17:05:32 +0900

    create custom eslint rule (steps 1 to 3 of webiny tutorial)

---
+++ eslint-test-project/eslint/index.js
@@ -0,0 +1,14 @@
+// eslint/index.js
+module.exports = {
+    rules: {
+        "no-template-literals": {
+            create: function(context) {
+                return {
+                    TemplateLiteral(node) {
+                        context.report(node, 'Do not use template literals');
+                    }
+                };
+            }
+        }
+    }
+};
